df697fc8b0bd4e997422dd2f32f77e491851505c
jglara <jglara@gmail.com>
2022-07-22 13:53:00 +0200

    Second solution absolute-permutation-hs

--- absolute-permutation-hs/app/Main.hs
+++ absolute-permutation-hs/app/Main.hs
@@ -4,15 +4,15 @@ import Control.Applicative ((<|>))
 import Data.Maybe (fromMaybe)
 
 
+
 solve1 :: [Int] -> [Int]
-solve1 [n,k] = fromMaybe [-1] $ solve n k (reverse [1..n]) []
+solve1 [n,0] = [1..n]
+solve1 [n,k] | (n `mod`k == 0) && even (n `div` k) = solve n k
 solve1 _ = [-1]
 
-solve :: Int -> Int -> [Int] -> [Int] -> Maybe [Int]
-solve _ _ [] acum = Just acum
-solve n k (x:rest) acum = let s1 = if x-k>0 && notElem (x-k) acum then solve n k rest ((x-k):acum) else Nothing
-                              s2 = if x+k<=n && notElem (x+k) acum then solve n k rest ((x+k):acum) else Nothing
-                          in s1 <|> s2
+solve :: Int -> Int -> [Int]
+solve n k = let mask = replicate k k ++ replicate k (-k)
+            in zipWith (+) [1..n] (cycle mask)
 
 main :: IO ()
 main = interact $ unlines . map ( unwords . map show . solve1 . map read . words) . drop 1 . lines
